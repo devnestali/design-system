b708fce9a3eace2e642201c6b6daa55e9d9d1fe3
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-06-17 11:08:55 +0200

    feat(storybook): Creating component Checkbox on react package from design system and documentate on storybox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Checkbox, Text } from '@call-ui/react'
+import type { CheckboxProps } from '@call-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: 'Una estilización de una checkbox reutilizable',
+      },
+    },
+  },
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
