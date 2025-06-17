4b690b55b4bf722e8cd46ca5df77566123f8c538
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-06-17 17:05:59 +0200

    chore(storybook): Installing addon-a11y to handle problems with accessibility

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: '@storybook/react-vite',
 }
 export default config
