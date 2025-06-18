48f6cff3c639e5b01f8f625fb6f484fb8db7478d
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-06-18 10:13:35 +0200

    fix(storybook): Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -4,5 +4,12 @@ const config: StorybookConfig = {
   stories: ['../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: '@storybook/react-vite',
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system'
+    }
+
+    return config
+  },
 }
 export default config
